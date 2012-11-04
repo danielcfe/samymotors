17a45f797a7bba5c130daa51028ca3d138603e83
Daniel Zambrano <xzdasx@gmail.com>
2012-11-04 16:11:50 -0430

    save config for producction

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://192.168.1.100/sammy/';
+$config['base_url']	= 'http://samymotors.pagodabox.com';
 
 /*
 |--------------------------------------------------------------------------
